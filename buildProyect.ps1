5cefbaef2d06c06ad1f0beea6e987272dc961d63
Antazx <guillermoantataz@gmail.com>
2020-02-17 18:34:07 +0100

    This is an automated commit

--- buildProyect.ps1
+++ buildProyect.ps1
@@ -1,8 +1,8 @@
-Remove-Item -Path ./server/public/* -Recurse
+Remove-Item -Path ./public/* -Recurse
 Set-Location -Path ./vue-frontend
 npm run build
 Set-Location -Path ..
-Copy-Item ./vue-frontend/dist/* -Recurse ./server/public/
+Copy-Item ./vue-frontend/dist/* -Recurse ./public/
 git add *
 git commit -m "This is an automated commit"
 git push origin master
