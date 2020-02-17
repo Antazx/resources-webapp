f3686ce1855aed11d2c466fadf2cfa71f8eab0d7
Antazx <guillermoantataz@gmail.com>
2020-02-17 19:01:10 +0100

    This is an automated commit

---
+++ vue-frontend/src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="6" lg="5" xl="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer />
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    v-model="username"
+                    :rules="usernameRules"
+                    label="Login"
+                    name="login"
+                    prepend-icon="mdi-account"
+                    type="text"
+                    required
+                  />
+
+                  <v-text-field
+                    v-model="password"
+                    :rules="passwordRules"
+                    label="Password"
+                    name="password"
+                    prepend-icon="mdi-lock"
+                    type="password"
+                    required
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-btn class="mb-1" color="primary" block>Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  props: {
+    source: String,
+    data: () => ({
+      username: '',
+      password: '',
+      usernameRules: [
+        (v) => !!v || 'Name is required',
+        (v) => v.length <= 10 || 'Name must be less than 10 characters'
+      ],
+      passwordRules: [(v) => !!v || 'Password is required']
+    })
+  }
+};
+</script>
